8ad3f58c68b0c3c653a13d0649016c2c04811244
Mendel <mkonikov123@gmail.com>
2021-07-28 19:51:23 -0400

    commit tree 8d5472aa2ec80fd6a8b4d1830ebb75cadf55b7ea

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const api = "http://localhost:6001"
+
+  useEffect(()=> {
+    fetch(api)
+    .then(resp => resp.json())
+    .then(data => data)
+  })
+  
   return (
     <div className="app">
       <Header />
